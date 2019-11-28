ced20cbbd42a280cb6a6d29bdb595af8874f9414
THEVlLLAlN <40323478@napier.ac.uk>
2019-11-28 18:28:33 +0000

    Database Connectivity

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import sqlite3
 app = Flask(__name__)
 
 
@@ -7,11 +8,12 @@ def account():
     logged = 0
     if request.method =='POST':
         print request.form
-        name = request.form['name']
-
-        return "<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in as %s!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/logout'>Logout</a></body></html>" % name
+        username = request.form['name']
+        password = request.form['password']
+        return "<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in as %s!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/logout'>Logout</a></body></html>" % username
 
     else:
+      
       page ="""
         <html>
         <head>
@@ -19,10 +21,15 @@ def account():
         <style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style>
         </head>
         <h1 >Dungeons and Dragons: HUB</h1>
-        <h2>Make a temporary username and log in!</h2>
+        <h2>Please Sign up in order to access this site. Already signed up? <a href='http://webtech-47.napier.ac.uk:5000/login'> Log in!</a></h2>
         <form action="" method="post" name="form">
-            <label for="name">Temporary Name</label>
+            <label for="name">Username</label>
+            
             <input type ="text" name="name" id="name"/>
+<br>
+            <label for"password">Password  </label>
+            <input type ="text" name="password" id="password">
+            <br>
             <input type="submit" name ="submit" id="submit"/>
         </html>
         """
@@ -32,9 +39,11 @@ def account():
 def logout():
      if request.method =='POST':
         print request.form
-        name = request.form['name']
+        username = request.form['name']
+        password = request.form['password']
+        return "<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in as %s!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/logout'>Logout</a></body></html>" % username
 
-        return "<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in as %s!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/logout'>Logout</a></body></html>" % name
+    
 
      else:
       page ="""
@@ -45,8 +54,11 @@ def logout():
         <h1 color="blue">Dungeons and Dragons: HUB</h1>
         <h2>Make a temporary username and log in!</h2>
         <form action="" method="post" name="form">
-            <label for="name">Temporary Name</label>
+            <label for="name">Username</label>
             <input type ="text" name="name" id="name"/>
+            <br>
+            <label for"password">Password  </label>
+            <input type ="text" name="password" id="password">
             <input type="submit" name ="submit" id="submit"/>
         </html>
         """
