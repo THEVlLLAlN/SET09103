d38da2f0f732574b606f9bc94aab266f546cd6a6
THEVlLLAlN <40323478@napier.ac.uk>
2019-11-28 23:09:52 +0000

    working login and posting system

--- hello.py
+++ hello.py
@@ -1,25 +1,44 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import sqlite3
 app = Flask(__name__)
-
+app.secret_key='eggs'
 @app.route("/makepost", methods=['POST','GET'])
 def make_post():
+    
+    if session['usernames']==None:
+        return redirect('http://webtech-47.napier.ac.uk:5000/')
     if request.method =='POST':
+        conn = sqlite3.connect('users.db')
+        cur = conn.cursor()
+        print request.form
+        post = request.form['post']
+        username = str(session['usernames'])
+
+        cur.execute("INSERT INTO posts (contents, poster) VALUES ('%s','%s')" %(post,username,))
+        conn.commit()
+        conn.close()
+
+
         return redirect('http://webtech-47.napier.ac.uk:5000/hub')
     else:
-        return ""
+        
+        return "<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/hub'>Back to the hub</a><br><br><form action='' method='post' name='form'> <label for='name'>Post</label> <input type ='text' name='post' id='post'/><input type='submit' name ='submit' id='submit'/> </body></html>" 
 
 
 @app.route("/hub", methods=['POST','GET'])
 def hub():
+    if session['loggedd']==None:
+        return redirect('http://webtech-47.napier.ac.uk:5000/')
+    else:
         conn = sqlite3.connect('users.db')
         cur = conn.cursor()
 
-        post_list =""
+        post_list ="<br>"
         cur.execute("SELECT * FROM posts")
         results = cur.fetchall()
+
         for row in results:
-            post_list = post_list + str(row) + "<br>"
+            post_list = post_list +"<div style='background-color:lightblue'>" + str(row) + "</div>" + "<br>"
 
         conn.commit()
         conn.close()
@@ -27,7 +46,7 @@ def hub():
  
 @app.route("/", methods=['POST','GET'])
 def account():
-    logged = 0
+    firstboot=0
     if request.method =='POST':
         print request.form
         username = request.form['name']
@@ -36,10 +55,13 @@ def account():
         cur = conn.cursor()
         cur.execute("INSERT INTO users (username, password) VALUES ('%s','%s')" %(username,password,))
         post_list =""
+        logged+=1
         cur.execute("SELECT * FROM posts")
         results = cur.fetchall()
         for row in results:
             post_list = post_list + str(row) + "<br>"
+        set_cookie('logged',1)
+        set_cookie('username','%s' %username)
 
         conn.commit()
         conn.close()
@@ -70,7 +92,8 @@ def account():
 
 @app.route("/logout", methods=['POST','GET'])
 def logout():
-     logged=0
+     session['usernames']=None
+     session['loggedd']=None
      if request.method =='POST':
         print request.form
         username = request.form['name']
@@ -78,7 +101,7 @@ def logout():
         conn = sqlite3.connect('users.db')
         cur = conn.cursor()
         for row in cur.execute("SELECT * FROM users WHERE username='%s' AND password='%s'" %(username,password,)): 
-            logged+=1
+            logged=1
             post_list =""
             cur.execute("SELECT * FROM posts")
             results = cur.fetchall()
@@ -86,6 +109,8 @@ def logout():
                 post_list = post_list + str(row) + "<br>"
 
              
+            session['usernames']=username
+            session['loggedd']=2
             return"<html><head><style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style></head><h1>Dungeons and Dragons: HUB</h1><h2>You are logged in!</h2><body><a href='http://webtech-47.napier.ac.uk:5000/logout'>Logout</a><br>Wanna join in? <a href='http://webtech-47.napier.ac.uk:5000/makepost'> Make your own post!</a><br>%s</body></html>" % post_list
         else:
             return" <html> <head> <style>html{background-color:#fefefe} body{font-family: Calibri; color:#454545; font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify} h1 { text-align: Center} a {color: #07a} a:visited {color: #FF7E47} textarea{resize: none;} .btn { border: none; background-color: inherit; padding: 14px 28px; font-size: 16px; cursor: pointer; display: inline-block; }/* Green */ .success { color: #07a; } .success:hover { background-color: #07a; color: white; }</style> </head> <h1 color='blue'>Dungeons and Dragons: HUB</h1> <h2>Make a temporary username and log in!</h2> <form action='' method='post' name='form'> <label for='name'>Username</label> <input type ='text' name='name' id='name'/> <br> <label for'password'>Password </label> <input type ='text' name='password' id='password'> <input type='submit' name ='submit' id='submit'/><br><body>There was no account with that credientials found, try again? </body> </html>" 
